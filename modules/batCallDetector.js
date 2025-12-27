fbcc9b0234aef6b6ede70cde5052b0954d365f15
pantongcut <pantong.cut@gmail.com>
2025-12-27 08:29:04 +0000

    if (powerDb < -130) continue;

--- modules/batCallDetector.js
+++ modules/batCallDetector.js
@@ -2243,7 +2243,7 @@ export class BatCallDetector {
         const powerDb = frame[b];
         
         // Ignore extremely low values (silence/padding) to avoid skewing mode
-        if (powerDb < -100) continue;
+        if (powerDb < -130) continue;
         
         const zoneKey = getZoneKey(freqHz);
         
